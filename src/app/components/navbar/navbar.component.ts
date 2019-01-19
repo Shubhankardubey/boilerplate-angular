c9ee6846d5f1c41b64dae2eef5003ca710390eec
Shubhankardubey <dubeyshubhankar007@gmail.com>
2019-01-19 19:27:48 +0530

    minor fixes

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -14,6 +14,7 @@ import {NgbDropdown} from '@ng-bootstrap/ng-bootstrap';
 export class NavbarComponent implements OnInit {
   isLogin: Boolean;
   firstName: any;
+  isCollapsed: any;
   constructor(
     private router: Router,
     private profileService: ProfileService
